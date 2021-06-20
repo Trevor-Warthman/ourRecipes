8dd463c859c3ec8e5907f0fdd941067ce99c7067
Trevor-Warthman <t.dubs371@gmail.com>
2021-06-20 17:47:38 -0400

    init firebase!

---
+++ ourrecipes/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDCWODNuWpO0ImWeM_Y4WYgKt3fL77ulfY",
+    authDomain: "ourrecipes-d576f.firebaseapp.com",
+    projectId: "ourrecipes-d576f",
+    storageBucket: "ourrecipes-d576f.appspot.com",
+    messagingSenderId: "396152375943",
+    appId: "1:396152375943:web:900bd76cf186ebb41b0622"
+}
