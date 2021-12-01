334c94ef38a1684acbd2afca1774ea85e4518bc8
Michael James Radey <radeymichael@gmail.com>
2021-12-01 14:04:44 -0500

    UI for Create Recipe with persistence mvp

--- ourrecipes/src/components/pages/CreateRecipe.vue
+++ ourrecipes/src/components/pages/CreateRecipe.vue
@@ -1,20 +1,28 @@
 <template>
-  <div> 
+  <div class="container"> 
     <NavBar :pages="createRecipe"></NavBar>
-    <p>Recipe Name: <input id="name" v-model="message" placeholder="enter recipe name"></p>
-    <p>Recipe Description: <textarea id="desc" v-model="message" placeholder="Enter recipe description"></textarea></p>
-    <p id="tagList">Tags: 
+    <p>Recipe Name: <input id="name" v-model="message" placeholder="Enter recipe name"></p>
+    <p>Recipe Description: <input id="desc" v-model="message" placeholder="Enter recipe description"></p>
+    <p id="tagList">Tags 
+      
       <input id="tag1" v-on:click="updateList" v-model="message" placeholder="Enter tag name">
     </p>
-    <p>Instructions: <textarea id="instruction" v-model="message" placeholder="Enter Recipe Instructions"></textarea></p>
-    <p>Ingredients: <textarea id="ingredient" v-model="message" placeholder="Enter Recipe Ingredients"></textarea></p>
-    <button v-on:click="submitRecipe">Add Recipe</button>
+    <p id="ingredientList">Ingredients: 
+      
+      <input id="ingredient1" v-on:click="updateList" v-model="message" placeholder="Enter ingredient name">
+    </p>
+    <p id="instructionList">Instructions: 
+      
+      <input id="instruction1" v-on:click="updateList" v-model="message" placeholder="Enter instruction name">
+    </p>
+    <button v-on:click="submitRecipe" class="createRecipe">Add Recipe</button>
   </div>
+  <p id="submitting"></p>
 </template>
 
 <script>
 import NavBar from "../NavBar.vue";
-// import db from '../firebaseInit'
+import db from '../firebaseInit'
 
 export default {
   recipeName: "Recipe",
@@ -22,6 +30,18 @@ export default {
   },
   components: {
     NavBar,
+  },
+  mounted() {
+    let initTag = document.getElementById('tag1');
+    initTag.addEventListener('click', () => this.updateList(initTag))
+
+    let initInstruction = document.getElementById('instruction1');
+    initInstruction.addEventListener('click', () => this.updateList(initInstruction))
+
+    let initIngredient = document.getElementById('ingredient1');
+    initIngredient.addEventListener('click', () => this.updateList(initIngredient))
+
+    document.addEventListener('click', this.handleClickAwayFromInputList, true);
   },
   data(){
     return{
@@ -31,7 +51,7 @@ export default {
       ingredients: [],
       description: '',
       tags: [],
-      found: true
+      tmpInp: null
     }
   },
   methods: {
@@ -39,28 +59,55 @@ export default {
       const data = {
         "name": document.getElementById("name").value,
         "description": document.getElementById("desc").value,
-        "tags": [document.getElementById("tag").value],
-        "instructions": [document.getElementById("instruction").value],
-        "ingredients": [document.getElementById("ingredient").value],
-        "authorUID":"1",
-        "created": Date.now()
+        "tags": [...document.getElementById('tagList').children].map(inp => inp.value),
+        "instructions": [...document.getElementById('instructionList').children].map(inp => inp.value),
+        "ingredients": [...document.getElementById('ingredientList').children].map(inp => inp.value),
+        "authorUID":"this-is-fake-for-now",
+        "created": (new Date()).toLocaleString()
       }
-      console.log(data)
-      //db.collection('recipes').doc('testUpload').set(data)
+      let submitMsg = document.getElementById('submitting');
+      submitMsg.innerHTML = 'Adding Recipe...';
+      db.collection('recipes').add(data).then(() => {
+        submitMsg.innerHTML = 'Added!';
+      }, err => {
+        submitMsg.innerHTML = 'Failed :(';
+        console.error(err);
+      })
     },
 
-    updateList: function(e) {
-      let clickedElement = e.srcElement;
+    updateList: function(clickedElement) {
       let parent = clickedElement.parentElement;
       let listType = parent.id.replace('List', '')
       let idx = clickedElement.id.replace(listType, '')
       if(idx == parent.childElementCount) {
         let newNode = document.createElement("input")
         newNode['id'] = listType + ((idx - 0) + 1);
-        newNode.setAttribute('onClick', "updateList");
-        newNode.setAttribute('placeholder', `Enter ${listType} name`);
+        newNode.setAttribute('placeholder', `Enter additional ${listType} name`);
+        newNode.setAttribute('class', 'fakeInp');
+
+        newNode.addEventListener('click', () => this.updateList(newNode));
         parent.appendChild(newNode)
+        if(this.tmpInp != null) {
+          this.tmpInp.setAttribute('class', '')
+        }
+        this.tmpInp = newNode
       }
+    }, 
+    handleClickAwayFromInputList(e) {
+      if(this.tmpInp != null) {
+        let parentNode = e.srcElement.parentElement;
+        if(parentNode.id !== this.tmpInp.parentElement.id) {
+          this.tmpInp.remove()
+        }
+      }
+    },
+    genRandomDocString: () => {
+      let result           = '';
+      let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      for (let i = 0; i < 20; i+=1) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return result;
     }
   }
 };
@@ -68,5 +115,41 @@ export default {
 </script>
 
 
-<style scoped>
+<style>
+input.fakeInp{
+  border: 1px dashed black;
+  background: transparent;
+}
+input {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+div.container {
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  margin: 10px;
+}
+
+button.createRecipe {
+  width: 100%;
+  background-color: rgb(75, 180, 206);
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer; 
+}
+
+button.createRecipe:hover {
+  background-color: rgb(75, 100, 206);
+  font-size: 15px;
+}
 </style>
