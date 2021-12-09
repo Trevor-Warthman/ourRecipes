ad6ae00bc16633121853a514189df3f6a2939a65
Michael James Radey <radeymichael@gmail.com>
2021-12-08 23:09:15 -0500

    added a timer cause why not

--- ourrecipes/src/components/pages/SignOut.vue
+++ ourrecipes/src/components/pages/SignOut.vue
@@ -1,5 +1,7 @@
 <template>
     you have been signed out!
+    <br>
+    Redirecting to home page in: {{this.secondsTilLogout}} seconds
 </template>
 
 <script>
@@ -8,6 +10,19 @@ import firebase from '../firebaseApp'
 export default {
     created() {
         firebase.auth().signOut();
+    },
+    mounted() {
+        setInterval(() => {
+            this.secondsTilLogout -= 1
+            if(this.secondsTilLogout == 0) {
+                this.$router.push("/")
+            }
+        }, 1000);
+    },
+    data() {
+        return {
+            secondsTilLogout: 5
+        }
     }
 };
 </script>
